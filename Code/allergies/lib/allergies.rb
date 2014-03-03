aea2cd148dcbbeb54cbf9b6423f554641b795348
Ali Abou-Zahr & Mark Lehman <student@epicodus.com>
2014-03-03 12:20:01 -0800

    changes code to use hashes insted of arrays

--- Code/allergies/lib/allergies.rb
+++ Code/allergies/lib/allergies.rb
@@ -1,14 +1,29 @@
 def allergies(score)
-  allergies = ["eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"]
+  allergies = {1 => "eggs", 2 => "peanuts", 4 => "shellfish", 8 => "strawberries", 16 => "tomatoes", 32 => "chocolate", 64 => "pollen", 128 => "cats"}
 
   has_allergies = []
-
   7.downto(0) do |num|
-    if score >= 2**num
-      has_allergies << allergies[num]
+    if score >= 2**num 
+      has_allergies << allergies[2**num]
       score -= 2**num
     end
   end
   return "Allergies: " + has_allergies.join(", ") + "."
 end
+allergies(270)
+
+
+# def allergies(score)
+#   allergies = ["eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"]
+
+#   has_allergies = []
+
+#   7.downto(0) do |num|
+#     if score >= 2**num
+#       has_allergies << allergies[num]
+#       score -= 2**num
+#     end
+#   end
+#   return "Allergies: " + has_allergies.join(", ") + "."
+# end
 
